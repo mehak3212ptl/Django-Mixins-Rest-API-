8ea89ab8f7cd71386e3930ea09820997d8547e36
mehak <mehakpatelattitude03@gmail.com>
2024-05-01 22:16:06 +0530

    first commit in mixins rest api

---
+++ project/app/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from .models import *
+from .serializers import *
+from rest_framework import mixins
+from rest_framework import generics
+
+class StudentList(mixins.ListModelMixin,
+                  mixins.CreateModelMixin,
+                  generics.GenericAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+class StudentDetail(mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    generics.GenericAPIView):
+    queryset = StudentModel.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
